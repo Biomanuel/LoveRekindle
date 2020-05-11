0bb46686234d3a9196f5853befd7b3c769198260
Biomanuel <31411274+Biomanuel@users.noreply.github.com>
2020-05-11 11:36:42 +0100

    Revert "Profile and chat ui"

--- app/src/main/java/com/reconciliationhouse/android/loverekindle/ui/recharge/RechargeViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.reconciliationhouse.android.loverekindle.ui.recharge;
-
-import androidx.lifecycle.ViewModel;
-
-public class RechargeViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
